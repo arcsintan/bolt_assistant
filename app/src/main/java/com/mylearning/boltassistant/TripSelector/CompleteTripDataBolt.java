c8df8047f037cfe1aefae935b444e1ef5ee54eb1
Alireza Sharifian <sharifian.isf@gmail.com>
2024-06-20 08:51:36 +0100

    new class to save all data related to a reserve

---
+++ app/src/main/java/com/mylearning/boltassistant/TripSelector/CompleteTripDataBolt.java
@@ -0,0 +1,33 @@
+package com.mylearning.boltassistant.TripSelector;
+
+public class CompleteTripDataBolt {
+    /*
+    - Class Name: android.view.ViewGroup, Text: null
+ -- Class Name: android.widget.ImageButton, Text: null
+-- Class Name: android.widget.TextView, Text: Request
+ -- Class Name: androidx.recyclerview.widget.RecyclerView, Text: null
+ --- Class Name: android.view.ViewGroup, Text: null
+---- Class Name: android.widget.TextView, Text: 18.13€ • Sun, 23 June, 08:00
+---- Class Name: android.widget.TextView, Text: Bolt
+---- Class Name: android.widget.TextView, Text: 23.7km
+---- Class Name: android.view.View, Text: null
+----- Class Name: android.view.View, Text: null
+----- Class Name: android.view.View, Text: null
+---- Class Name: android.widget.RelativeLayout, Text: null
+---- Class Name: android.widget.TextView, Text: Near Rua Calvet de Magalhães, 2770, Paço de Arcos
+---- Class Name: android.widget.TextView, Text: Near Rua dos Eucaliptos, Laranjeiro
+--- Class Name: android.view.ViewGroup, Text: null
+---- Class Name: android.widget.TextView, Text: Exact locations will be shown before starting the Scheduled Ride
+--- Class Name: android.widget.FrameLayout, Text: null
+--- Class Name: android.view.ViewGroup, Text: null
+---- Class Name: android.widget.TextView, Text: 23.7km • 22 min
+--- Class Name: android.view.ViewGroup, Text: null
+---- Class Name: android.widget.TextView, Text: Extra waiting time
+---- Class Name: android.widget.TextView, Text: free 2 min included in the fare, 0.33€/min after 2 min
+--- Class Name: android.view.ViewGroup, Text: null
+---- Class Name: android.widget.TextView, Text: SASHA COELHO
+---- Class Name: android.widget.TextView, Text: 8 rides
+-- Class Name: android.widget.LinearLayout, Text: null
+--- Class Name: android.widget.TextView, Text: Accept
+     */
+}
