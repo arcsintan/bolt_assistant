11db54517d1b1f2889c5e6d31443245706a53e36
Alireza Sharifian <sharifian.isf@gmail.com>
2024-06-23 08:32:50 +0100

    A new command class to read text in all depth.

---
+++ app/src/main/java/com/mylearning/boltassistant/ReadAllTextInAllDepthCommand.java
@@ -0,0 +1,48 @@
+package com.mylearning.boltassistant;
+
+public class ReadAllTextInAllDepthCommand implements Command {
+    private final int typeTag=4;
+    private MyAccessibilityService service;
+    private RectangleData rectangleData;
+
+
+    public ReadAllTextInAllDepthCommand(MyAccessibilityService service, RectangleData rectangleData) {
+        this.rectangleData = rectangleData;
+        this.service = service;
+    }
+
+    public ReadAllTextInAllDepthCommand(MyAccessibilityService service) {
+        this.service = service;
+    }
+    @Override
+    public void execute() {
+        service.extractAllTextInAllDepth(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (service.lock) {
+                    service.lock.notify(); // Notify the waiting thread that the operation is complete
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getTimeUntilNextCommand() {
+        return 0;
+    }
+
+    @Override
+    public int getTypeTag() {
+        return typeTag;
+    }
+
+    @Override
+    public CircleData getCircleData() {
+        return null;
+    }
+
+    @Override
+    public RectangleData getRectangleData() {
+        return rectangleData;
+    }
+}
